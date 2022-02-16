b5fc13657243618734cd6f906d2538870d6be139
mrs <martinsvalerd@gmail.com>
2022-02-16 19:09:53 +0100

    Introduced url.c for constructing the request URL.

---
+++ src/url.c
@@ -0,0 +1,22 @@
+#include "url.h"
+#include "met-data-fetcher.h"
+#include <stdio.h>
+
+int typeNow = 0;
+int typeForecast = 1;
+
+char URL[100];
+
+void urlFormatter(void) {
+
+  char *URLs[2] = {
+      "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=%f&lon=%f",
+      "https://api.met.no/weatherapi/locationforecast/2.0/"
+      "complete?lat=%f&lon=%f"};
+
+  if (typeNow) {
+    sprintf(URL, URLs[0], lat, lon);
+  } else {
+    sprintf(URL, URLs[1], lat, lon);
+  }
+}
---
+++ src/url.h
@@ -0,0 +1,4 @@
+void urlFormatter(void);
+extern char URL[100];
+extern int typeNow;
+extern int typeForecast;
