6c49a726fbaa70f550f2640d8fc6d89fc2414af4
mrs <martinsvalerd@gmail.com>
2022-03-07 13:32:40 +0100

    Code for dealing with the redirection of stdout to a file and then back to normal again.

--- src/met-data-fetcher.c
+++ src/met-data-fetcher.c
@@ -1,6 +1,7 @@
 #include "met-data-fetcher.h"
 #include "url.h"
 #include <curl/curl.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,7 +16,7 @@ int met_data_fetcher() {
   CURL *curl = curl_easy_init();
 
   if (!curl) {
-    fprintf(stderr, "init failed\n");
+    fprintf(stderr, "curl init failed\n");
     exit(1);
   }
 
@@ -23,8 +24,10 @@ int met_data_fetcher() {
   curl_easy_setopt(curl, CURLOPT_URL, URL);
   curl_easy_setopt(curl, CURLOPT_USERAGENT, "Dark Ninja Dragon/1.0");
 
-  // Make stdout go to data.json (storing the response).
-  freopen("data.json", "w", stdout);
+  // Send stdout to file
+  int fd = dup(STDOUT_FILENO);
+  if (!freopen("/tmp/MET-weather-data.json", "w", stdout))
+    fprintf(stderr, "Error: %d", errno);
 
   // Fetch the data
   CURLcode result = curl_easy_perform(curl);
@@ -33,6 +36,11 @@ int met_data_fetcher() {
   }
 
   curl_easy_cleanup(curl);
+
+  // Restore stdout.
+  fflush(stdout);
+  close(STDOUT_FILENO);
+  dup2(fd, STDOUT_FILENO);
 
   return 0;
 }
